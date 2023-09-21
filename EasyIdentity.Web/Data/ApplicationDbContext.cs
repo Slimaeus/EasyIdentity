fee8affa5f205a8ffbdfa51801ae70f5f8298703
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-21 16:24:33 +0700

    Setup Identity for ApplicationDbContext and Add Migration

--- EasyIdentity.Web/Data/ApplicationDbContext.cs
+++ EasyIdentity.Web/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using EasyIdentity.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyIdentity.Core.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
