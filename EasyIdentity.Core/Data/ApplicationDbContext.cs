6fd13c125b4330f305635eebc6a177d0a1bd7b6d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-21 16:01:30 +0700

    Add project files.

---
+++ EasyIdentity.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EasyIdentity.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyIdentity.Core.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Room> Rooms { get; set; }
+}
---
+++ EasyIdentity.Core/Entities/Room.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyIdentity.Core.Entities;
+
+public class Room
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
