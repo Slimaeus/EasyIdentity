3682263e7ecabb261b7ed84351558202c39ce1bc
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-21 16:39:37 +0700

    Add UserService and IUserService

---
+++ EasyIdentity.Core/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyIdentity.Core.Services;
+public interface IUserService
+{
+    Task<IdentityResult> Login(string userName, string password);
+    Task<IdentityResult> Register(string userName, string email, string password);
+}
---
+++ EasyIdentity.Core/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyIdentity.Core.Services;
+
+public class UserService : IUserService
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public UserService(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+    public async Task<SignInResult> Login(string userName, string password)
+    {
+        var user = await _signInManager.UserManager
+            .FindByNameAsync(userName);
+
+        if (user == null)
+            return SignInResult.Failed;
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+
+        return result;
+    }
+
+    public async Task<IdentityResult> Register(string userName, string email, string password)
+    {
+        var user = new IdentityUser
+        {
+            UserName = userName,
+            Email = email
+        };
+        var result = await _signInManager.UserManager.CreateAsync(user, password);
+        return result;
+    }
+}
